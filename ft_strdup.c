558a008843bdacc25653d89cb355ddc425920334
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-17 15:25:44 -0700

    ft_strdup

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/17 12:17:05 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/17 15:23:31 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*strdup(const char *s1)
+{
+	int		len;
+	char	*s2;
+
+	len = ft_strlen(s1);
+	s2 = (char *)malloc(len * sizeof(char));
+	if (s2 == NULL)
+		return NULL;
+	while (len > 0)
+	{
+		s2[len - 1] = s1[len - 1];
+		len--;
+	}
+	return (s2);
+}
