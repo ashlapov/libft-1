516c6ae843a853d50ad3447e32f92c6e409c02de
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-17 16:31:17 -0700

    add ifndef

--- libft.h
+++ libft.h
@@ -6,9 +6,12 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 21:18:44 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/17 15:57:02 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/09/17 16:18:31 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
 
 #include <string.h>
 
@@ -18,3 +21,4 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isalpha(int c);
 int		ft_isalnum(int c);
+#endif
