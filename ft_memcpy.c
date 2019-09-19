ede5e78461b942f097876de93567448d69fbfcbd
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-19 11:48:07 -0700

    add (char *) to vars

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 19:34:46 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/18 10:44:17 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/09/19 11:35:06 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char		*d;
 	const char	*s;
 
-	d = dst;
-	s = src;
+	d = (char *)dst;
+	s = (char *)src;
 	while (n--)
 		*d++ = *s++;
 	return (dst);
