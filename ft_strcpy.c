0392f987e56a321ce1d5a38183c3e01cbc1b4bba
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-19 11:29:43 -0700

    updated with memcpy

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 16:42:35 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/17 18:25:43 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/09/19 11:29:04 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,5 @@
 
 char	*ft_strcpy(char *dst, const char *src)
 {
-	int		len;
-
-	len = 0;
-	while (src[len] != '\0')
-		len++;
-	dst[--len] = '\0';
-	while (len >= 0)
-	{
-		dst[len] = src[len];
-		len--;
-	}
-	return (dst);
+	return (ft_memcpy(dst, src, ft_strlen(src) + 1));
 }
