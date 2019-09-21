5b6d5b58837bb7caee4e6e03135e8aef3e5eb2cf
Konstantin Gavrilov <kgavrilo@e1z3r6p13.42.us.org>
2019-09-21 15:30:23 -0700

    putchar, putstr, putnbr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 21:18:44 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/17 19:36:06 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/09/21 15:20:06 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define HEADER_FILE_NAME
 
 #include <string.h>
+#include <unistd.h>
 
 int		ft_strlen(const char *s);
 char	*ft_strdup(const char *s1);
@@ -25,4 +26,8 @@ int		ft_isprint(int c);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strcat(char *restrict s1, const char *restrict s2);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+void	ft_putnbr(int n);
+
 #endif
