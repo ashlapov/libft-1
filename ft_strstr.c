ecffa7fc3a9b460a9f00dab2322ae79a34a9f466
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-26 09:19:14 -0700

    strstr delete main and comments

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 17:37:58 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/25 07:28:57 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/09/26 09:17:14 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,9 @@ char	*ft_strstr(const char *haystack, const char *needle)
 {
 	int		i;
 	int		j;
-	//int		res;
 	char	*result;
 
 	i = 0;
-	// find first letter
-	// try to find needle
-
 	while (haystack[i] != '\0')
 	{
 		j = 0;
@@ -38,14 +34,4 @@ char	*ft_strstr(const char *haystack, const char *needle)
 		i++;
 	}
 	return (NULL);
-}
-
-#include <stdio.h>
-
-int		main(void)
-{
-	char	needle[4] = "hool";
-	char	haystack[21] = "hello from school 42";
-	printf("%s\n", ft_strstr(haystack, needle));
-	return(0);
 }
