a6a985c49b98caeeded4f134f545a9d6d03f5201
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-29 10:30:03 -0700

    strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/26 10:41:23 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/27 10:35:48 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	while (dst[i] != '\0' && i < dstsize)
+		i++;
+	j = i;
+	while (src[i - j] != '\0' && i < dstsize - 1)
+	{
+		dst[i] = src[i - j];
+		i++;
+	}
+	if (j < dstsize)
+		dst[i] = '\0';
+	return (j + ft_strlen(src));
+}
