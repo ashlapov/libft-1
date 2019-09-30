18f4466e73a00ec8001e45265749c9837fac5541
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-09-29 21:45:58 -0700

    strnew

---
+++ ft_strnew.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/29 21:12:21 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/29 21:20:04 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char	*result;
+
+	result = (char *)malloc(sizeof(char) * size);
+	if (result == NULL)
+		return (NULL);
+	while (size >= 0)
+	{
+		result[size] = '\0';
+		size--;
+	}
+	return (result);
+}
