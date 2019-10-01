2d4759552c87a3f33492b083f216a7d60c59b333
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-01 15:56:37 -0700

    strmap

---
+++ ft_strmap.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 13:40:47 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/30 13:46:08 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	char	*result;
+	int		len;
+
+	len = ft_strlen(s);
+	result = (char *)malloc(sizeof(char) * len);
+	if (result == NULL)
+		return (NULL);
+	while (len >= 0)
+	{
+		result[len] = f(s[len]);
+	}
+	return (result);
+}
