4ebad72bf9c05fc21425006fe809d8bc3bbd60af
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-01 15:56:28 -0700

    memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 10:54:31 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/30 12:58:09 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*str1;
+	const unsigned char	*str2;
+
+	str1 = (const unsigned char *)s1;
+	str2 = (const unsigned char *)s2;
+
+	if (s1 == s2 || n == 0)
+		return (0);
+	while (n--)
+	{
+		if (*str1 != *str2)
+			return (str1 - str2);
+		if (n > 0)
+		{
+			str1++;
+			str2++;
+		}
+	}
+	return (0);
+}
