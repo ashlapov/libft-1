34d03e05d747957e49df38fb060af8cb34f48b84
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-01 15:56:07 -0700

    memalloc

---
+++ ft_memalloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/30 13:31:23 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/09/30 13:39:01 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = malloc(size);
+	if (mem = NULL)
+		return (NULL);
+	ft_bzero(mem, size);
+	return (mem);
+}
