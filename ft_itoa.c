6327357081b6d733eaf44e04fa3b2a5576d6643c
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-03 10:22:11 -0700

    itoa

---
+++ ft_itoa.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/03 09:58:48 by kgavrilo          #+#    #+#             */
+/*   Updated: 2019/10/03 10:00:02 by kgavrilo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	itoa_isnegative(int *n, int *negative)
+{
+	if (*n < 0)
+	{
+		*n *= -1;
+		*negative = 1;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	int		tmpn;
+	int		len;
+	int		negative;
+	char	*str;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	tmpn = n;
+	len = 2;
+	negative = 0;
+	itoa_isnegative(&n, &negative);
+	while (tmpn /= 10)
+		len++;
+	len += negative;
+	if ((str = (char*)malloc(sizeof(char) * len)) == NULL)
+		return (NULL);
+	str[--len] = '\0';
+	while (len--)
+	{
+		str[len] = n % 10 + '0';
+		n = n / 10;
+	}
+	if (negative)
+		str[0] = '-';
+	return (str);
+}
