375e8335ef16863c713503ef0a9a2e9dd349bc4b
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-03 10:22:04 -0700

    isprint

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,9 +6,11 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 16:34:04 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/17 16:39:55 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/03 10:03:21 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int		ft_isprint(int c)
 {
