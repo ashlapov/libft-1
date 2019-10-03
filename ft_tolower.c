e8b9a1ad1e040a8886297ffd81482400a734dd79
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-03 10:23:37 -0700

    tolower

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,13 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 13:22:57 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/01 15:58:26 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/03 10:12:52 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_tolower(int c)
+int		ft_tolower(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (c + 32);
