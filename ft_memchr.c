cc87a3cb9fd10036460781ea18276c5e8f2f8dfd
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-03 15:32:35 -0700

    fix error

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 10:43:14 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/03 12:11:52 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/03 15:30:13 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,11 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	unsigned int	i;
 
 	i = 0;
-	while (((unsigned const char *)s)[i] != '\0' && i < n)
+	while (i < n)
+	{
 		if (((unsigned const char *)s)[i] == (unsigned char)c)
 			return ((void *)&((unsigned const char *)s)[i]);
+		i++;
+	}
 	return (NULL);
 }
