0c55746b9bfd9cbc617e429dd781fc90a13e6936
Konstantin Gavrilov <kgavrilo@e1z2r7p42.42.us.org>
2019-10-03 10:23:06 -0700

    strncat

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,13 +6,13 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 10:21:53 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/26 10:40:14 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/03 10:15:27 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncat(char *restrict s1, const char *restrict s2, size_t n)
+char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
 	ft_strncpy(s1 + ft_strlen(s1), s2, n);
 	return (s1);
