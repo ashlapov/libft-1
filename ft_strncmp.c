9c0d0b7f9a1dcf4a1335c9137fa91b28e49a401d
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-04 14:26:19 -0700

    fix error

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/24 14:45:14 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/03 12:13:17 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/04 13:13:44 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,5 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < n - 1)
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
