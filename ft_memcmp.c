97afb4472b8ef5d31d900a640ab044ab17464c0a
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-06 11:09:54 -0700

    memcmp fix error

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 10:54:31 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/03 11:02:49 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/06 11:08:35 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	while (n--)
 	{
 		if (*str1 != *str2)
-			return (str1 - str2);
+			return (*str1 - *str2);
 		if (n > 0)
 		{
 			str1++;
