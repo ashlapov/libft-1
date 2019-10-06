c93b91fcbb8ca922c9d6d2add74f75a860a907d9
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-05 21:01:01 -0700

    fix error

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/23 19:28:31 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/03 12:07:09 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/05 20:58:27 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ void	*ft_memset(void *b, int c, size_t len)
 	unsigned char	*ret;
 
 	ret = (unsigned char *)b;
+	c = (unsigned char)c;
 	while (len--)
 	{
-		*ret = (unsigned char)c;
-		if (len)
-			ret++;
+		*ret = c;
+		ret++;
 	}
-	return (ret);
+	return (b);
 }
