f89c4f4c8ed7c36d20b9feaebb1aa087f9cd5b60
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-05 21:17:33 -0700

    strsub error fix

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 13:58:49 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/30 14:03:38 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/05 21:16:35 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,17 @@
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
 	char	*result;
+	size_t	i;
 
 	result = (char *)malloc(sizeof(char) * (len - start));
 	if (result == NULL)
 		return (NULL);
-	while (start < len)
+	i = 0;
+	while (i < len)
 	{
-		result[start] = s[start];
+		result[i] = s[start];
 		start++;
+		i++;
 	}
 	return (result);
 }
