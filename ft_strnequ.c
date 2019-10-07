e8f2f274f287a2ac3c8d4d889e5408a289f4f0f4
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-06 20:43:11 -0700

    ft_strneque

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 13:56:07 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/03 14:42:00 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/06 20:40:42 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,9 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	unsigned int	i;
-
-	if (s1 && s2 && n)
-	{
-		i = 0;
-		while (s1[i] != '\0' && i < n)
-		{
-			i++;
-			if (s1[i] != s2[i])
-				return (0);
-		}
+	if (s1 == NULL || s2 == NULL)
+		return (0);
+	if (ft_strncmp(s1, s2, n) == 0)
 		return (1);
-	}
 	return (0);
 }
