37d3c2c1bfaac50bdb04afc71dc687814df85e6a
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-07 21:23:07 -0700

    strjoin fix error

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 20:37:52 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/29 20:55:01 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/07 21:22:41 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,27 @@
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*result;
+	int		i;
+	int		j;
 
 	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (result == NULL)
 		return (NULL);
-	result = ft_strcat(result, s1);
-	result = ft_strcat(result, s2);
+	j = 0;
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		result[j] = s1[i];
+		i++;
+		j++;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		result[j] = s2[i];
+		i++;
+		j++;
+	}
+	result[j] = '\0';
 	return (result);
 }
