d8c68497db08847ad6c41a4b787f186d95a29d67
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-09 15:47:03 -0700

    strequ

--- ft_strequ.c
+++ ft_strequ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strequ.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 21:36:14 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/29 21:42:39 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/09 15:42:36 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	int	i;
-
-	if (s1 && s2)
+	if (!s1 || !s2)
+		return (0);
+	if (ft_strlen(s1) != ft_strlen(s2))
+		return (0);
+	while (*s1 && *s2)
 	{
-		i = 0;
-		while (s1[i] != '\0')
-		{
-			i++;
-			if (s1[i] != s2[i])
-				return (0);
-		}
-		return (1);
+		if (*s1 != *s2)
+			return (0);
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (1);
 }
