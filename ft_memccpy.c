44301171cf380ae19beafb52e2bd7cd823f982d6
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-08 20:16:11 -0700

    memccpy memcpy fix

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/27 10:36:58 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/28 09:06:59 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/08 19:42:17 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
+	char	*ptr;
 	size_t	i;
 
-	i = 0;
-	if (dst == NULL || src == NULL)
-		return (NULL);
-	while (i < n)
+	i = -1;
+	ptr = dst;
+	while (++i < n)
 	{
-		((char *)dst)[i] = ((char *)src)[i];
-		if (((char *)src)[i] == c)
-			return ((char *)dst + i + 1);
-		i++;
+		*(ptr + i) = *((unsigned char *)src + i);
+		if (*((unsigned char *)src + i) == (unsigned char)c)
+			return (dst + i + 1);
 	}
 	return (NULL);
 }
+
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgavrilo <kgavrilo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/17 19:34:46 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/09/19 11:35:06 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/08 19:46:18 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*d;
-	const char	*s;
+	unsigned int		i;
 
-	d = (char *)dst;
-	s = (char *)src;
-	while (n--)
-		*d++ = *s++;
+	i = -1;
+	if (dst == NULL && src == NULL && n)
+		return (NULL);
+	while (++i < n)
+		((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
 	return (dst);
 }
