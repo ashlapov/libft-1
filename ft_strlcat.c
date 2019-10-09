2746ef8d832cd79a66d08c81d06883f9c4f14a2a
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-09 15:22:22 -0700

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/26 10:41:23 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/08 20:06:44 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/09 15:18:55 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,12 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
-	size_t	j;
+	size_t len;
 
-	i = 0;
-	if (!dstsize)
-		return (i);
-	while (dst[i] != '\0' && i < dstsize)
-		i++;
-	j = i;
-	while (src[i - j] != '\0' && i < dstsize - 1)
-	{
-		dst[i] = src[i - j];
-		i++;
-	}
-	if (j < dstsize)
-		dst[i] = '\0';
-	return (j + ft_strlen(src));
+	len = ft_strlen(dst);
+	if (dstsize > len)
+		ft_strncat(dst, src, (dstsize - len - 1));
+	if (dstsize < len)
+		return (dstsize + ft_strlen(src));
+	return (len + ft_strlen(src));
 }
