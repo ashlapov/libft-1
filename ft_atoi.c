1f967e5ab3a7edd0e7f338fc0bd2e158d6d9cac3
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-09 15:22:08 -0700

    atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/22 14:35:17 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/07 20:51:42 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/09 15:19:26 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,22 @@
 
 int		ft_atoi(const char *str)
 {
-	int	ret;
+	int	result;
 	int	negative;
 
 	negative = 1;
-	ret = 0;
-	while (*str && (*str == '\t' || *str == '\n' || *str == '\v'
-	|| *str == '\f' || *str == '\r' || *str == ' '))
+	result = 0;
+	while (*str && (*str == ' ' || *str == '\n' || *str == '\t' ||
+			*str == '\v' || *str == '\f' || *str == '\r'))
 		++str;
-	if (negative == 1 && *str == '-')
+	if (*str == '-')
 		negative = -1;
 	if (*str == '-' || *str == '+')
 		++str;
 	while (*str && *str >= '0' && *str <= '9')
 	{
-		if (ret > 469762049 && negative == 1)
-			return (-1);
-		else if (ret > 469762049 && negative == -1)
-			return (0);
-		ret = (ret * 10) + *str - '0';
+		result = result * 10 + (*str - 48);
 		++str;
 	}
-	return (ret * negative);
+	return (result * negative);
 }
