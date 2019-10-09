5de59365e53f98691e730dee1a739abcc99d9b7f
Konstantin Gavrilov <gavrilovks@gmail.com>
2019-10-09 15:22:14 -0700

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: kgavrilo <kgavrilo@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/29 20:37:52 by kgavrilo          #+#    #+#             */
-/*   Updated: 2019/10/07 21:22:41 by kgavrilo         ###   ########.fr       */
+/*   Updated: 2019/10/09 15:05:00 by kgavrilo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,16 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
-	int		i;
-	int		j;
+	char	*str;
+	int		len;
 
-	result = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (result == NULL)
+	if (!s1 || !s2)
 		return (NULL);
-	j = 0;
-	i = 0;
-	while (s1[i] != '\0')
-	{
-		result[j] = s1[i];
-		i++;
-		j++;
-	}
-	i = 0;
-	while (s2[i] != '\0')
-	{
-		result[j] = s2[i];
-		i++;
-		j++;
-	}
-	result[j] = '\0';
-	return (result);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	str = (char *)malloc(sizeof(*str) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strcpy(str, s1);
+	ft_strcat(str, s2);
+	return (str);
 }
